bd30a61dcc5c4b0fd888144faa97a5084f1ccffc
Araceli271115 <marlenixgc07@gmail.com>
2019-06-03 00:26:06 -0500

    Funcionalidad Perfil
    Funcionalidad  del fragment perfil del maestro

--- Informa_TEC/app/src/main/java/com/example/informa_tec/Modelo/Conexion.java
+++ Informa_TEC/app/src/main/java/com/example/informa_tec/Modelo/Conexion.java
@@ -1,5 +1,5 @@
 package com.example.informa_tec.Modelo;
 
 public class Conexion {
-    public static final String servidor = "http://192.168.1.67:8000/api/";
+    public static final String servidor = "http://192.168.1.104:8000/api/";
 }
